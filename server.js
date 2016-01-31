2b7ca33c62343d882d8a1182b02ce2fbf5065efb
Axa <axa@macbook-pro-de-axa.home>
2016-01-31 18:10:06 +0100

    web tuto initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var app = express();
+
+var port = 8080;
+
+var middleware = require('./middleware.js');
+
+app.use(middleware.logger);
+
+
+ // app.get('/', function (req, res) {
+ // 	res.send('Hello express!');
+ // });
+
+app.get('/about', middleware.requireAuthentication, function (req, res) {
+ 	res.send('About us');
+ });
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(port, function () {
+	console.log('Express server started on port ' + port + '.');
+});
